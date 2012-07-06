5ea5961958a400d6a9f492785e224322ee108f57
lfelliott <lfelliott@hotmail.com>
2012-07-06 14:38:35 -0500

    slight reformat and added leading comment

--- R_model_steps_chunks.R
+++ R_model_steps_chunks.R
@@ -1,8 +1,10 @@
+# Script to run boosted regression tree on fish data and apply predictions from model to full stream layer in chunks to reduce memory consumption
+# Lee Elliott 20120706
+
 library(gbm)
 library(dismo)
 
 redshiner<- read.csv("D:/AWR/redshiner.csv")
-
 
 # fit the model
 redshiner.tc1.lr1 <- gbm.step(data=redshiner, gbm.x= 4:61, gbm.y=3, family = "bernoulli", tree.complexity = 1, learning.rate = 0.1, bag.fraction =0.5)
@@ -47,9 +49,6 @@ head(redshiner.df)
 colnames(RS.df) <- c("HUC4_COMID", "resid")
 library(foreign)
 write.dbf(redshiner.df, file = " D:/AWR/Out/redshiner_residuals.dbf")
-
-
-
 
 # To change both column names at once . . .
 # colnames(PD.df) <- c("Seg_ID", "resid")
