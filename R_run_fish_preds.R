fea319ed17e37f8eda85aa911973c121937fa341
lfelliott <lfelliott@hotmail.com>
2012-08-10 15:23:02 -0500

    Corrected some typos

--- R_run_fish_preds.R
+++ R_run_fish_preds.R
@@ -1,4 +1,5 @@
 # Script to apply predictions from boosted regression tree on fish data to models held in folder
+# Originally ran into problems with the field names being lower case in the data used to build the model and upper case in the streams set. used ?toupper to correct
 # Lee Elliott 20120810
 
 library(gbm)
@@ -7,17 +8,17 @@ library(foreign)
 
 working_dir <- "D:/AWR/directory_of_models"
 streams<-read.delim("D:/AWR/strms4.txt", header=TRUE, sep="\t")
+setwd(working_dir)
 
 chunks <- 10
 numrows <- dim(streams)[1]
 jump <- round(numrows/chunks) + 1
 
-setwd(working_dir)
 fish_list <- list.files(pattern = "*.RData")
 for (fish_data in fish_list)
 	{
 	load(fish_data)
-	fish_name <- strsplit(fish_data, ".", fixed = TRUE)[[1]][1])
+	fish_name <- strsplit(fish_data, ".", fixed = TRUE)[[1]][1]
 	# get predictions for chunks of streams file
 	for (i in seq(from=1, to=numrows, by=jump))
 		{
@@ -33,11 +34,12 @@ for (fish_data in fish_list)
 		else
 			{
 			temp.df <- data.frame(stream_subset$HUC4_COMID, preds)
-			fishresult.df <- rbind(fishrsult.df, data.frame(stream_subset$HUC4_COMID, preds))
+			fishresult.df <- rbind(fishresult.df, data.frame(stream_subset$HUC4_COMID, preds))
 			}
 		}
 	colnames(fishresult.df)[1] <- "HUC4_COMID"
-	write.dbf(fishresult.df, file = paste(working_dir, "/", fish_name, ".dbf" ))
-	rm(fish_data)
+	dbfname <- paste(working_dir, "/", fish_name, ".dbf", sep = "")
+	write.dbf(fishresult.df, file = dbfname)
+	rm(modres)
 	}
 	
