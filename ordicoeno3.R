6ae920e19615b43786b11bec68540d0eab3418c0
Lee Elliott <lfelliott@hotmail.com>
2013-01-02 12:44:25 -0600

    Include modest modifications

--- ordicoeno3.R
+++ ordicoeno3.R
@@ -12,7 +12,7 @@
 # x.subset <- x[, keep.species]
 
 
-ordicoeno2 <- function (x, ordiplot, axis = 1, ...) 
+ordicoeno3 <- function (x, ordiplot, axis = 1, ...) 
 {
     if (!require(mgcv)) {
         stop("Requires package mgcv")
@@ -20,7 +20,7 @@ ordicoeno2 <- function (x, ordiplot, axis = 1, ...)
 	species <- NULL
 	linecolor <- NULL
 	linetype <- NULL
-    ordiscore <- scores(ordiplot, display = "sites")[, axis]
+    ordiscore <- scores(ordiplot, display = "sites")[Or, axis]
     original <- cbind(x, ordiscore)
     sorted <- original
     seq <- order(ordiscore)
@@ -29,36 +29,32 @@ ordicoeno2 <- function (x, ordiplot, axis = 1, ...)
     names(edfs) <- colnames(x)
 #   palette(rainbow(ncol(x)))
 	colorsloaded <- rep(palette(), 20)
-	gamresult <- gam(sorted[, 1] ~ s(ordiscore), data = sorted)
-    edfs[1] <- summary(gamresult)$edf
     newdata <- data.frame(seq(min(sorted$ordiscore), max(sorted$ordiscore), 
         length = 1000))
     colnames(newdata) <- "ordiscore"
+
+
 	lmax <- NULL
 	lmaxscore <- NULL
+	gamresults <- NULL
+	gamresults2 <- NULL
 	for (i2 in 1:ncol(x)){
 		lgamresult <- gam(sorted[, i2] ~ s(ordiscore), data = sorted)
+		gamresults[[i2]] <- lgamresult
 		lgamresult2 <- predict(lgamresult, newdata)
+		gamresults2[[i2]] <- lgamresult2
 		lmax <- c(lmax, max(lgamresult2))
 		lmaxscore <- c(lmaxscore,((which(lgamresult2 == max(lgamresult2), arr.ind=T)[1]/1000)*(abs(min(sorted$ordiscore))+max(sorted$ordiscore))) + min(sorted$ordiscore))
 		}
 	curvemax = max(lmax)
 
-    gamresult2 <- predict(gamresult, newdata)
-    plot(newdata$ordiscore, gamresult2, type = "l", ylim = c(0, 
-        curvemax), col = 1, pch = 1, xlab = "site score on ordination axis", 
-        ylab = "species values", ...)
-	cat("Species\t\tColor\t\t\tMaximum Value\tedf\n")
-	if (nchar(colorsloaded[1]) > 5) cat(colnames(original)[1], "\t\t", colorsloaded[1],"\t\t", max(gamresult2), "\t", lmaxscore[1], "\n") else cat(colnames(original)[1], "\t\t", colorsloaded[1],"\t\t\t", max(gamresult2), "\t", lmaxscore[1],"\n")
-	species <- c(species, colnames(original)[1])
-	linecolor <- c(linecolor, 1)
-	linetype <- c(linetype, 1)
-	for (i in 2:ncol(x)) {
-        gamresult <- gam(sorted[, i] ~ s(ordiscore), data = sorted)
-        gamresult2 <- predict(gamresult, newdata)
+	cat("Species\t\tColor\t\t\tMaximum Value\tScore of Max\n")
+	for (i in 1:ncol(x)) {
+        gamresult <- gamresults[[i]]
+        gamresult2 <- gamresults2[[i]]
         edfs[i] <- summary(gamresult)$edf
-        points(newdata$ordiscore, gamresult2, type = "l", pch = 19, 
-            col = i, ...)
+		if (i == 1) plot(newdata$ordiscore, gamresult2, type = "l", ylim = c(0,curvemax), col = 1, pch = 1, xlab = "site score on ordination axis", ylab = "species values", ...)
+		else points(newdata$ordiscore, gamresult2, type = "l", pch = 19, col = i, ...)
 		if (nchar(colorsloaded[i]) > 5) cat(colnames(original)[i], "\t\t", colorsloaded[i],"\t\t", max(gamresult2), "\t", lmaxscore[i], "\n") else cat(colnames(original)[i], "\t\t", colorsloaded[i],"\t\t\t", max(gamresult2), "\t", lmaxscore[i],"\n")
 		species <- c(species, colnames(original)[i])
 		linecolor <- c(linecolor, i)
