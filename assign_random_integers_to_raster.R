aa1c24db5d556e50294c50e682ebe10505258431
Lee Elliott <lfelliott@hotmail.com>
2013-03-11 13:11:59 -0500

    Added determine frequency from stack

--- assign_random_integers_to_raster.R
+++ assign_random_integers_to_raster.R
@@ -3,5 +3,7 @@ r1 <- raster(ncol=10, nrow=10)
 r2 <- r1
 values(r1) <- sample(1:5, ncell(r1), replace=T)
 values(r2) <- sample(2:6, ncell(r2), replace = T)
-df <- as.data.frame(stack(r1, r2))
+s <- stack(r1, r2)
+df <- as.data.frame(s)
 table(df)
+freq(s, merge=T)
