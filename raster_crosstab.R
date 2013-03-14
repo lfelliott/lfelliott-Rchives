8e6bb803689ccc8e365319f414f1fcef9bab7d19
Lee Elliott <lfelliott@hotmail.com>
2013-03-14 15:26:23 -0500

    Added making long format from crosstab.

--- raster_crosstab.R
+++ raster_crosstab.R
@@ -3,3 +3,7 @@ r1 <- raster("lc_segap")
 r2 <- raster("lc_segap")
 r_xtab <- crosstab(r1, r2)
 write.csv(r_xtab, file='r_xtab.csv')
+# produce a long format comparison of crosstab
+r_xtabdf <- as.data.frame(r_xtab)
+colnames(r_xtabdf) <- c("r1", "r2", "freq")
+
