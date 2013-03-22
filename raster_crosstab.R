60fa33f13074d8a7c7b0e5bafabc963e62f8b5a7
Lee Elliott <lfelliott@hotmail.com>
2013-03-22 09:10:30 -0500

    changed written csv to data frame of crosstab (long format)

--- raster_crosstab.R
+++ raster_crosstab.R
@@ -1,9 +1,10 @@
 library(raster)
-r1 <- raster("lc_segap")
-r2 <- raster("lc_segap")
+r1 <- raster("anderson.img", format = "HFA")
+r2 <- raster("evt.img", format = "HFA")
 r_xtab <- crosstab(r1, r2)
-write.csv(r_xtab, file='r_xtab.csv')
 # produce a long format comparison of crosstab
 r_xtabdf <- as.data.frame(r_xtab)
 colnames(r_xtabdf) <- c("r1", "r2", "freq")
+write.csv(r_xtabdf, file='anderson_x_evt.csv')
+
 
