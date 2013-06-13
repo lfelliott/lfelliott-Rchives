dce93e39ac94deeef744807c2cd98fb024e0f0ef
Lee Elliott <lfelliott@hotmail.com>
2013-06-13 10:33:45 -0500

    Function that returns latitude, longitude from georeferenced image.

---
+++ imglatlong.R
@@ -0,0 +1,13 @@
+# Function that returns latitude and longitude from an image file
+imglatlong <- function(imgname)
+	{
+	require(rgdal)
+	x <- GDALinfo(imgname)
+	xattr <- attr(x, "mdata")
+	exiflong <- xattr[grep("EXIF_GPSLongitude=", xattr)]
+	exiflat <- xattr[grep("EXIF_GPSLatitude=", xattr)]
+	long <- as.numeric(gsub("^.*=\\(([0-9\\.]+)\\).*","\\1",exiflong)) + as.numeric(gsub("^.*=\\([0-9\\.]+\\) \\(([0-9\\.]+)\\).*","\\1",exiflong))/60 + as.numeric(gsub("^.*=\\([0-9\\.]+\\) \\([0-9\\.]+\\) \\(([0-9\\.]+)\\).*","\\1",exiflong))/3600
+	long <- -(long)
+	lat <- as.numeric(gsub("^.*=\\(([0-9\\.]+)\\).*","\\1",exiflat)) + as.numeric(gsub("^.*=\\([0-9\\.]+\\) \\(([0-9\\.]+)\\).*","\\1",exiflat))/60 + as.numeric(gsub("^.*=\\([0-9\\.]+\\) \\([0-9\\.]+\\) \\(([0-9\\.]+)\\).*","\\1",exiflat))/3600
+	return(c(lat, long))
+	}
