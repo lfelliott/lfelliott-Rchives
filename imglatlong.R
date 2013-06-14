161739ea519680efb0ee00fac65d90c4570c6259
Lee Elliott <lfelliott@hotmail.com>
2013-06-14 13:32:56 -0500

    Added require rgdal and corrected for W and S hemispheres

--- imglatlong.R
+++ imglatlong.R
@@ -7,7 +7,8 @@ imglatlong <- function(imgname)
 	exiflong <- xattr[grep("EXIF_GPSLongitude=", xattr)]
 	exiflat <- xattr[grep("EXIF_GPSLatitude=", xattr)]
 	long <- as.numeric(gsub("^.*=\\(([0-9\\.]+)\\).*","\\1",exiflong)) + as.numeric(gsub("^.*=\\([0-9\\.]+\\) \\(([0-9\\.]+)\\).*","\\1",exiflong))/60 + as.numeric(gsub("^.*=\\([0-9\\.]+\\) \\([0-9\\.]+\\) \\(([0-9\\.]+)\\).*","\\1",exiflong))/3600
-	long <- -(long)
 	lat <- as.numeric(gsub("^.*=\\(([0-9\\.]+)\\).*","\\1",exiflat)) + as.numeric(gsub("^.*=\\([0-9\\.]+\\) \\(([0-9\\.]+)\\).*","\\1",exiflat))/60 + as.numeric(gsub("^.*=\\([0-9\\.]+\\) \\([0-9\\.]+\\) \\(([0-9\\.]+)\\).*","\\1",exiflat))/3600
+	if (any(grep("EXIF_GPSLongitudeRef=W", xattr))) long <- -(long)
+	if (any(grep("EXIF_GPSLatitudeRef=S", xattr))) lat <- -(lat)
 	return(c(lat, long))
 	}
