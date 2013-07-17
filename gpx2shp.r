08c794af6408bb84796067c75b85e5026cdb99dd
lfelliott <lfelliott@hotmail.com>
2013-07-17 15:53:19 -0500

    Simple script to convert gpx to shapefile

---
+++ gpx2shp.r
@@ -0,0 +1,10 @@
+library(rgdal)
+setwd("d:/r/spatial")
+tracks <- readOGR("peckranch.gpx", layer='tracks')
+writeOGR(tracks, getwd(), "peckranch", driver="ESRI Shapefile")
+
+#proj4string(PointsAsFrame) <- CRS("+proj=longlat +datum=WGS84")
+
+#The variants are as described in the readOGR help file, that is layer=
+#"waypoints", "tracks", "routes", "track_points", or "route_points". Maybe 
+#reading the help page might save time??
