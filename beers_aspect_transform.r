3d31afd18728a0229d3e1f320b5053870a5143c5
lfelliott <lfelliott@hotmail.com>
2014-07-22 10:28:08 -0500

    modified algorithm, 2 possibilities exist

--- beers_aspect_transform.r
+++ beers_aspect_transform.r
@@ -4,4 +4,6 @@ library(aspace)
 aspect <- raster("hosp_aspect")
 r <- aspect
 r2 <- setValues(r, getValues(sin_d(r + 45) + 1))
+r3 <- setValues(r, getValues(cos_d(45 - r) + 1))
 r2[aspect < 0] <- -1
+writeRaster(r3, "hosp_beers_cos.img", format='HFA')
